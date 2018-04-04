0c923272ed14e749395a49a97eea5868a1173e96
Saurav <sp.gharti@gmail.com>
2018-04-04 18:53:33 +0000

    add package django-shop

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-shop', 'url': 'http://pypi.python.org/pypi/django-shop/0.11.3'},
 	{'name':'django-oscar', 'url': 'http://pypi.python.org/pypi/django-oscar/1.5.1'},
 	{'name':'django-guardian', 'url': 'http://pypi.python.org/pypi/django-guardian/1.4.9'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
