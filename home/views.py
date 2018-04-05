e2b7a50e871eb96c642f4efe5cb8d73158abb8e5
Saurav <sp.gharti@gmail.com>
2018-04-05 15:06:24 +0000

    remove package django-app-metrics

--- home/views.py
+++ home/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
-	{'name':'django-app-metrics', 'url': 'http://pypi.python.org/pypi/django-app-metrics/0.9.0'},
 	{'name':'django-analytical', 'url': 'http://pypi.python.org/pypi/django-analytical/2.4.0'},
 	{'name':'django-shop', 'url': 'http://pypi.python.org/pypi/django-shop/0.11.3'},
 	{'name':'django-guardian', 'url': 'http://pypi.python.org/pypi/django-guardian/1.4.9'},
